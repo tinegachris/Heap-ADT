b28d717673e32522fd53d0c06d99398c213de1d0
Chrispine Tinega <72353423+tinegachris@users.noreply.github.com>
2021-12-15 18:56:56 +0300

    Update config.h

--- config.h
+++ config.h
@@ -3,5 +3,5 @@
  * Activate test #N by defining the corresponding LAB11_TESTN to have the value 1.
  */
 
-#define LAB11_TEST1	0		// Programming Exercise 3: writeLevels
+#define LAB11_TEST1	1	// Programming Exercise 3: writeLevels
 
